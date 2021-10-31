15a9d3a6d53cd949011de1864d74c6162676678b
Alejandro Hernandez <alejandrojaez@outlook.com>
2021-10-30 19:20:32 -0500

    add print logo

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import typing
 from pprint import pprint
 from flask import Flask, jsonify, request
 from escpos.printer import Win32Raw
@@ -12,10 +13,19 @@ def hello_world(path):
     p = Win32Raw()
     p.set(font='a')
     j = request.get_json()
+    p.set(align='center')
+    p.image('celfun.png', high_density_vertical=True, high_density_horizontal=True)
+    p.set(align='center', bold=False, double_height=False, double_width=False)
+    p.text("\n\nHEZC850515PY1\n")
+    p.text("Col. Cuernavaca Centro c.p. 62000\n")
+    p.text("Cuernavaca, Morelos\n")
+    p.text("******************************************\n\n")
     for product in j:
         leftright(p, left=product["desc"], right=product["price"], font='a')
+
+    p.text("\n******************************************\n\n")
     p.barcode('{B7503028490974', 'CODE128', function_type="B", width=2)
-    p.qr("https://celfun.com", size=6)
+    p.qr("https://celfun.com", size=8)
     p.cut()
     p.close()
 
