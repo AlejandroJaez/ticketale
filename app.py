1661b984f7cbc815721f117c3d4776731278f462
Alejandro Hernandez <alejandrojaez@outlook.com>
2021-11-04 15:48:45 -0600

    add repair receipt

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import typing
 from pprint import pprint
 from flask import Flask, jsonify, request
-from escpos.printer import Win32Raw
+from escpos.printer import Win32Raw,Usb
 from utils import leftright
+from sys import platform as _platform
+
 
 app = Flask(__name__)
+type = 'reparacion'
 
 
 @app.route("/", methods=["POST"], defaults={"path": ""})
 @app.route("/<path:path>", methods=["POST"])
 def hello_world(path):
-    p = Win32Raw()
+    if _platform == "win64" or _platform == "win32":
+        p = Win32Raw()
+    else:
+        p = Usb(0x04b8, 0xa700)
     p.set(font='a')
     j = request.get_json()
     p.set(align='center')
@@ -19,13 +25,29 @@ def hello_world(path):
     p.text("\nHEZC850515PY1\n")
     p.text("Col. Cuernavaca Centro c.p. 62000\n")
     p.text("Cuernavaca, Morelos\n")
+
+    p.set(align='center', bold=True, double_height=True, double_width=True)
+    p.text("ORDEN DE SERVICIO\n")
+    p.set(align='center', bold=False, double_height=False, double_width=False)
     p.text("******************************************\n\n")
-    for product in j:
-        leftright(p, left=product["desc"], right=product["price"], font='a')
+    if type == 'venta':
+        for product in j:
+            leftright(p, left=product["desc"], right=product["price"], font='a')
+    if type == 'reparacion':
+        leftright(p, left="Cliente", right="Alejandro Hernandez", font='a')
+        leftright(p, left="Equipo:", right="Apple iPhone 7 negro", font='a')
+        leftright(p, left="Imei:", right="355316082611279", font='a')
+        leftright(p, left="Falla:", right="Display roto", font='a')
+        leftright(p, left="Notas:", right="", font='a')
+        p.text("No se pudo verificar funcionamiento del equipo.\n")
+        leftright(p, left="Presupuesto:", right="$950.00", font='a')
 
     p.text("\n******************************************\n\n")
-    p.barcode('{B7503028490974', 'CODE128', function_type="B", width=2)
+    p.barcode('{B00000089', 'CODE128', function_type="B", width=2)
+    p.set(align='center', font='b')
+    p.text("Verifique si su equipo ya esta reparado escaneando el siguiente codigo:")
     p.qr("https://celfun.com", size=8)
+    p.text("Consulte garantia y aviso de privacidad en celfun.com")
     p.cut()
     p.close()
 
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 def leftright(p, left, right, font='a'):
     if font == 'a':
         p.set(font='a')
-        p.textln("{:<30}{:>12}".format(left, right))
+        p.textln("{:<21}{:>21}".format(left, right))
     else:
         p.set(font='b')
         p.textln("{:<44}{:>12}".format(left, right))
